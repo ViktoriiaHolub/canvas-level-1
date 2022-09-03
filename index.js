0cbb5b98f89fee4fefec9775b2c37b20ef160914
ViKtoriiaHolub <victoria_holub@epam.com>
2022-09-03 18:45:28 +0300

    work with lines and figures 1. part 2 of 7

--- index.js
+++ index.js
@@ -1,2 +1,52 @@
 let canvas = document.querySelector("canvas");
 let ctx = canvas.getContext("2d"); // ctx (context)
+
+// notes
+// in canvas direction:
+// x - (right direction x _>)
+// y - (down direction y |v)
+
+// line
+ctx.beginPath();
+ctx.moveTo(50, 50); // args is x, y
+ctx.lineTo(150, 50);
+ctx.stroke();
+
+// arrow down
+ctx.beginPath();
+ctx.moveTo(50, 150);
+ctx.lineTo(100, 200);
+ctx.lineTo(150, 150);
+ctx.stroke();
+
+// triangle
+ctx.beginPath();
+ctx.moveTo(50, 300);
+ctx.lineTo(100, 350);
+ctx.lineTo(150, 300);
+ctx.lineTo(50, 300);
+ctx.stroke();
+
+// square
+ctx.beginPath();
+ctx.moveTo(50, 450);
+ctx.lineTo(150, 450);
+ctx.lineTo(150, 550);
+ctx.lineTo(50, 550);
+ctx.closePath(); // return to the beginner position in moveTo
+ctx.stroke();
+
+// triangle using method closePath()
+ctx.beginPath();
+ctx.moveTo(50, 650);
+ctx.lineTo(100, 700);
+ctx.lineTo(150, 650);
+ctx.closePath();
+ctx.stroke();
+
+// triangle with background color using fill and without stroke() and closePath()
+ctx.beginPath();
+ctx.moveTo(50, 800);
+ctx.lineTo(100, 850);
+ctx.lineTo(150, 800);
+ctx.fill();
